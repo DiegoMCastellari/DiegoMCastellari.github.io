552d8c8daa62d22ae1c4ad92dd89bba25d246c09
DiegoMCastellari <agrimcastellari@gmail.com>
2023-11-08 08:41:28 -0300

    indexes calc

--- views/rasterian/rasterian_sidebar.js
+++ views/rasterian/rasterian_sidebar.js
@@ -23,7 +23,27 @@ var rasterian_sidebar = {
             ['conv_image', 'focal_conv_image.html']
         ],
         'local_indexes': [
-            ['local_indexes', 'local_indexes.html']
+            ['calculate_index', 'focal_local_index_calculate.html'],
+            ['index_avi', 'focal_local_index_avi.html'],
+            ['index_baem', 'focal_local_index_baem.html'],
+            ['index_bsi', 'focal_local_index_bsi.html'],
+            ['index_bu', 'focal_local_index_bu.html'],
+            ['index_evi', 'focal_local_index_evi.html'],
+            ['index_gndvi', 'focal_local_index_gndvi.html'],
+            ['index_msi', 'focal_local_index_msi.html'],
+            ['index_nbri', 'focal_local_index_nbri.html'],
+            ['index_ndbi', 'focal_local_index_ndbi.html'],
+            ['index_ndgi', 'focal_local_index_ndgi.html'],
+            ['index_ndmi', 'focal_local_index_ndmi.html'],
+            ['index_ndsi', 'focal_local_index_ndsi.html'],
+            ['index_ndvi', 'focal_local_index_ndvi.html'],
+            ['index_ndwi', 'focal_local_index_ndwi.html'],
+            ['index_ui', 'focal_local_index_ui.html']
+        ],
+        'local_math': [
+            ['index_norm', 'focal_local_math_norm.html'],
+            ['index_ratio', 'focal_local_math_ratio.html'],
+            ['calc_index', 'focal_local_math_calc_index.html']
         ]
     },
     "Plot Img": {
