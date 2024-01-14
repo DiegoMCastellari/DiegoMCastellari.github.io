fd9a4a50ce177909a66739c8889fa23abdc8b2b9
DiegoMCastellari <agrimcastellari@gmail.com>
2024-01-14 17:13:24 -0300

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,11 @@
+/* 
+IGEO - GCS: documentación de proceso Python
+
+
+*/
+
+
+
 var projectList = [
     /* ,
     {
@@ -9,27 +17,27 @@ var projectList = [
         "page": "surveyingThays.html"
     } */ 
     {
-        "name": "Pluspetrol",
+        "name": "Footprint Detection",
         "description": "Human activity footprint detection with raster data.",
         "nombre": "Pluspetrol",
-        "descripcion": "Detectión de intervención por actividad humana a partir de datos raster.",
+        "descripcion": "Human activity footprint detection with raster data.",
         "image": "../src/image/projects/20_Pluspetrol_FootprintDetection/logo.JPG",
         "page": "20_Pluspetrol_FootprintDetection.html"
     },
     {
-        "name": "MetroGas: SD Protocol",
+        "name": "SD Protocol",
         "description": "Data cleanning and processing as requiered by ENARGAS SD Protocol",
         "nombre": "MetroGas: Protocolo SD",
-        "descripcion": "Limpieza y procesamiento de datos según requerimientos del Protocolo SD de ENARGAS",
-        "image": "http://drive.google.com/uc?export=view&id=1cLP7RKITjYe_YBJF4JDKx80QDtzkjY9g",
-        "page": "False"
+        "descripcion": "Data cleanning and processing as requiered by ENARGAS SD Protocol",
+        "image": "../src/image/projects/19_MetroGas_ProtocoloSD/logo.JPG",
+        "page": "19_MetroGas_ProtocoloSD.html"
     },
     {
-        "name": "CEEU - CII",
+        "name": "CEEU - BID PUENTE",
         "description": "Local Goverment KPIs calculation",
         "nombre": "CEEU - CIC",
         "descripcion": "Calculo de índices a nivel de gobiernos locales",
-        "image": "http://drive.google.com/uc?export=view&id=1uhX9qLMNkUFEuj4VAhs198DEWREq0Apa",
+        "image": "../src/image/icon/comingsoon.jpg",
         "page": "False"
     },
     {
@@ -37,7 +45,7 @@ var projectList = [
         "description": "Green public spaces study for ACUMAR",
         "nombre": "CEEU - ACUMAR",
         "descripcion": "Estudio de espacios verdes públicos para ACUMAR",
-        "image": "http://drive.google.com/uc?export=view&id=1PLtexHU-Uh8BK5tfAk4UCNGuULlluBfL",
+        "image": "../src/image/icon/comingsoon.jpg",
         "page": "False"
     },
     {
@@ -45,95 +53,95 @@ var projectList = [
         "description": "Local Goverment KPIs calculation",
         "nombre": "CEEU - MOP",
         "descripcion": "Calculo de índices a nivel de gobiernos locales",
-        "image": "http://drive.google.com/uc?export=view&id=1CTUb9bzWsbyD_SSWg4eaGBcurnuSB9B3",
+        "image": "../src/image/icon/comingsoon.jpg",
         "page": "False"
     },
     {
         "name": "CVSA",
-        "description": "Road Inventory App Development",
+        "description": "Road Inventory App Design",
         "nombre": "CVSA",
-        "descripcion": "Desarrollo de una app de inventario vial",
-        "image": "http://drive.google.com/uc?export=view&id=1Lx97-N6DxV8wjEC4V9FAHjbBz0hI_XHp",
-        "page": "11_IGEO_CVSA_AppInv.html"
+        "descripcion":  "Road Inventory App Design", /* "Desarrollo de una app de inventario vial", */
+        "image": "../src/image/projects/15_IGEO_CVSA_AppInv/popup.JPG",
+        "page": "15_IGEO_CVSA_AppInv.html"
     },
     {
         "name": "CVSA",
         "description": "Road Damage Detection on GoPro Video",
         "nombre": "CVSA",
-        "descripcion": "Detección de daños en pavimento a partir de un video de GoPro",
-        "image": "http://drive.google.com/uc?export=view&id=1jPs5ov8DPTTeebyAMHfA1Fn9dKyLP5Vg",
-        "page": "10_IGEO_VideoProcess.html"
+        "descripcion": "Road Damage Detection on GoPro Video",
+        "image": "../src/image/projects/14_IGEO_CVSA_VideoProcess/machine learning.JPG",
+        "page": "14_IGEO_CVSA_VideoProcess.html"
     },
     {
         "name": "CVSA",
         "description": "Road Images Dataset Exploration",
         "nombre": "CVSA",
-        "descripcion": "Exploración de un dataset de imágenes", 
-        "image": "http://drive.google.com/uc?export=view&id=1T7byu6msX2gJrURT_jCatBQRzdKJlXoK",
-        "page": "9_IGEO_imageClean.html"
+        "descripcion": "Road Images Dataset Exploration", 
+        "image": "../src/image/projects/13_IGEO_CVSA_imageClean/intro.JPG",
+        "page": "13_IGEO_CVSA_imageClean.html"
     },
     {
         "name": "Road Engineering Project",
-        "description": "Drone survey for a road design project design in Neuquén (Arg) - with: Mario Marol (TM)",
+        "description": "Drone survey for a road design project design in Neuquén (Arg)",
         "nombre": "Proyecto de Ingeniería de Ruta",
-        "descripcion": "Relevamiento drone para un proyecto de ruta en Neuquén (Arg)",
-        "image": "http://drive.google.com/uc?export=view&id=1sfbwolYumZwF5q23weWlkoVHmJWVVdhN",
-        "page": "False"
+        "descripcion": "Drone survey for a road design project design in Neuquén (Arg)",
+        "image": "../src/image/projects/12_TM_campana_neuquen/gps.JPG",
+        "page": "12_TM_campana_neuquen.html"
     },
-    {
+    /* {
         "name": "Voltage Line Engineering Project",
         "description": "Drone survey for a high voltage line in La Rioja (Arg) - with: Mario Marol (TM)",
         "nombre": "Proyecto de línea de alta tensión",
         "descripcion": "Relevamiento drone para una línea de alta tensión en La Rioja (Arg)",
-        "image": "http://drive.google.com/uc?export=view&id=15omkEvUS2pkEBt6xb_WpH6my01Wk8er1",
+        "image": "../src/image/projects/",
         "page": "False"
-    },
+    }, */
     {
         "name": "Limay River Bathimetry",
-        "description": "Limay river survey for hidrography model - Neuquén (Arg) - with: Mario Marol (TM)",
-        "nombre": "Batimetría del río Limay",
-        "descripcion": "Relevamiento del río Limay para modelo hidrográfico - Neuquén (Arg)",
-        "image": "http://drive.google.com/uc?export=view&id=1Ht2uDrHdJ6bM9pbE0Zi4LC4n3Q00ZWfX",
-        "page": "False"
+        "description": "Limay river survey for hidrography model - Neuquén (Arg)",
+        "nombre": "Limay River Bathimetry",
+        "descripcion": "Limay river survey for hidrography model - Neuquén (Arg)",
+        "image": "../src/image/projects/11_TM_campana_limay/limay_boat.JPG",
+        "page": "11_TM_campana_limay.html"
     },
     {
         "name": "Estudio Thays",
         "description": "Suveying for a Landscape Design Project",
         "nombre": "Estudio Thays",
-        "descripcion": "Relevamiento topográfico para proyecto de paisajismo",
-        "image": "http://drive.google.com/uc?export=view&id=1ZcIKAst2pof-Lb8pY1Us8enjR00AASHe",
-        "page": "5_Thays.html"
+        "descripcion": "Suveying for a Landscape Design Project",
+        "image": "../src/image/projects/09_Thays/thays.JPG",
+        "page": "09_Thays.html"
     },
-    {
+/*     {
         "name": "Dam Engineering Project",
         "description": "Drone survey for a dam engineering project in Catamarca (Arg) - with: Mario Marol (TM)",
         "nombre": "Proyecto de Represa",
         "descripcion": "Relevamiento drone para un proyecto de represa en Catamarca (Arg)",
         "image": "http://drive.google.com/uc?export=view&id=1jrDJ2e3XfP-p-UdJS4FG-6z-mwjDF39M", 
         "page": "False"
-    },
+    }, */
     {
         "name": "LAS Classification",
         "description": "Drone LiDAR point cloud processing",
-        "nombre": "Clasificación de Nube de Puntos",
-        "descripcion": "Procesamiento de nube de puntos LiDAR capturada desde Drone.",
-        "image": "http://drive.google.com/uc?export=view&id=1A0ieyyiuM3BGtAyL2-_bV1EICOccOyAa",
-        "page": "3_droneservices.html"
+        "nombre": "LAS Classification",
+        "descripcion": "Drone LiDAR point cloud processing",
+        "image": "../src/image/projects/07_droneservices/pc.JPG",
+        "page": "07_droneservices.html"
     },
     {
         "name": "Nilus",
         "description": "Censal data analysis and visualization",
         "nombre": "Nilus",
         "descripcion": "Análisis y visualización de datos censales",
-        "image": "http://drive.google.com/uc?export=view&id=1IFSvMt-DQlfm4WmC4vTXTuAnCqhF5INY",
-        "page": "2_Nilus.html"
+        "image": "../src/image/projects/06_Nilus/nilus.JPG",
+        "page": "06_Nilus.html"
     },
-    {
+    /* {
         "name": "Road Survey for Highway",
         "description": "Topographic survey for a highway project - with: Mario Marol (TM)",
         "nombre": "Relevamiento de Ruta para Autopista",
         "descripcion": "descripcion",
         "image": "http://drive.google.com/uc?export=view&id=1eWNAgtpAbsi30-P6nm-ksmX5RvM9vR6X",
         "page": "False"
-    }    
+    }   */  
 ]
